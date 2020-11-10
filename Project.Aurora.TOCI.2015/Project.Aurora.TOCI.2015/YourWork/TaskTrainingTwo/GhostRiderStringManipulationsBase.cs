1f6eb029a6127e923d66812755333b52da2760a2
Mateusz Jelen <mateuszjelen@gmail.com>
2020-11-10 20:40:04 -0300

    Training tasks todo added. 1. String Manipulation 2. Pesel Validation

---
+++ Project.Aurora.TOCI.2015/Project.Aurora.TOCI.2015/YourWork/TaskTrainingTwo/GhostRiderStringManipulationsBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Aurora.TOCI._2015.YourWork.TaskTrainingTwo
+{
+    public abstract class GhostRiderStringManipulationsBase
+    {
+        // true: beata; at, ata, ta, ea
+        // false: beata; ae, eb, aa
+        protected abstract bool IsStringInString(string subject, string seek);
+
+        // true: beata; aat, taae, aea
+        // false: beata; aaa, taaa, tta, aee
+        protected abstract bool IsStringElementsInString(string subject, string seek);
+
+        // true: beata; ataeb, aateb, tbaea
+        // false: beata; aaateb, fdwgdw
+        protected abstract bool IsStringAnagramOfString(string subject, string seek);
+
+        protected abstract string GetNick();
+
+        public abstract StringManipulationsResults RunStringOperations(string subject, string seek);
+
+        protected virtual StringManipulationsResults Run(string subject, string seek)
+        {
+            var result = new StringManipulationsResults();
+
+            result.Nick = GetNick();
+            result.Subject = subject;
+            result.Seek = seek;
+
+            result.Type = this.GetType();
+
+            result.IsAnagram = IsStringAnagramOfString(subject, seek);
+            result.IsStringElementsInString = IsStringElementsInString(subject, seek);
+            result.IsStringInString = IsStringInString(subject, seek);
+
+            return result;
+        }
+    }
+}
---
+++ Project.Aurora.TOCI.2015/Project.Aurora.TOCI.2015/YourWork/TaskTrainingTwo/PeselValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Aurora.TOCI._2015.YourWork.TaskTrainingTwo
+{
+    public abstract class PeselValidator
+    {
+        protected abstract string CutOffDate(string pesel);
+
+        protected abstract bool Checksum(string pesel);
+
+        protected abstract bool ValidateDate(int year, int month, int day);
+
+        public abstract string GetNick();
+
+        // cut, checksum, validate
+        public abstract bool IsPeselValid(string pesel);
+    }
+}
