7fdec3bc50061f3b6883cce60274b6c60b60582c
Mateusz Jelen <mateuszjelen@gmail.com>
2021-02-05 13:40:41 +0100

    Codewars tasks Lazy Repeater (5kyu) - Added solution with test. Solution has been passed all codewars tests.

---
+++ Project.Aurora.TOCI.2015/CodeWars.Excersize/Lazy Repeater/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Excersize.Lazy_Repeater
+{
+    public class Kata
+    {
+        public static Func<char> MakeLooper(string str)
+        {
+            int idx = 0;
+            //do obczajenia jak sie zmienia idx dodaje addwatch na ten w returnie
+            return () => str[idx++ % str.Length];
+        }
+    }
+}
---
+++ Project.Aurora.TOCI.2015/Project.Aurora.Tests/Codewars tests/TestsForRepeaterLooperTask.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars.Excersize.Lazy_Repeater;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Aurora.Tests.Codewars_tests
+{
+    [TestClass]
+    public class TestsForRepeaterLooperTask
+    {
+        [TestMethod]
+        public void WhatIsThisShitActuallyDoing()
+        {
+            //objekt ktory jest metoda klasy kata, kazde jego wywolanie result() wywoluje ten func. 
+            // w tym funcu jest iterator ktory jest na stale przypisany do result(bo przeciez to instancja) i dlatego po kazdym wywolaniu mamy inna litere
+            var result  = Kata.MakeLooper("string");
+            var a = result();
+            var b = result();
+            var c = result();
+        }
+    }
+}
